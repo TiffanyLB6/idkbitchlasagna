1202995f9d18715b3afa7950c1fce5e95568bf9c
TiffanyLB6 <tlombardo@est.ups.edu.ec>
2024-02-16 16:16:46 -0500

    Add(II)

--- src/main/java/com/ups/poo/controller/TestController.java
+++ src/main/java/com/ups/poo/controller/TestController.java
@@ -19,9 +19,38 @@ public class TestController {
         return message;
     }
 
-    @GetMapping("/concat/{name}/{lastname}/{age}")
-    public String concatenate(@PathVariable String name, @PathVariable String lastname, @PathVariable int age){
+    @GetMapping("/concat-2")
+    public String concatenate(@RequestParam String name,
+                              @RequestParam (required = false) String lastname,
+                              @RequestParam (required = false) Integer age){
+        String message = "This is my first SpringBootProject, my name is: " + name;
+
+        if (lastname != null) {
+            return message = message + "my last name is " + lastname;
+        }
+        if (age != null){
+            return message = message + " and my age is " + age;
+        }
+        return message;
+    }
+    /*@GetMapping("/concat/{name}/{lastname}")
+    public String pollofrito1(@PathVariable String name, @PathVariable String lastname) {
         return "This is my second rest service!, and my name is: "
-                + name + " " + lastname + " and my age is: " + age;
+                + name + " " + lastname;
+    }
+    */
+    @GetMapping("/purple/{name}/{lastname}/{age}")
+    public String purple(@PathVariable String name,
+                              @PathVariable (required = false) String lastname,
+                              @PathVariable (required = false) Integer age) {
+        String message = "This is my first SpringBootProject, my name is: " + name;
+
+        if (lastname != null) {
+            message = message + " my last name is " + lastname;
+        }
+        if (age != null){
+            message = message + " and my age is " + age;
+        }
+    return message;
     }
 }
